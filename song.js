22c35218eaa1cb0261e546be8b341b4025b0b75e
vizzaare <32402531+vizzaare@users.noreply.github.com>
2019-05-01 09:05:09 -0400

    first commit

---
+++ song.js
@@ -0,0 +1,28 @@
+/**********************************
+ * Attributes of my favorite song.*
+ **********************************/
+
+// song obj
+var songObj = {
+  name: "The unforgiven",
+  artist: "Metallica",
+  album: "Metallica",
+  genre: "Heavy Metal",
+  length: "6:27",
+  label: "Elektra",
+  songWriters: ["James Hetfield", "Kirk HammettLars", "Ulrich"],
+  Producers: ["Bob Rock", "James Hetfield", "Lars Ulrich"],
+  year: 1991
+};
+
+// display object content on console
+console.log(songObj);
+
+// display song's name
+console.log(songObj.name);
+
+// display artist
+console.log(songObj.artist);
+
+// display producers
+console.log(songObj.Producers);
